46320c8266d47d3852b9e979ffb17253e0348aac
James Boyce <61768977+jdbsquire@users.noreply.github.com>
2025-08-28 19:00:26 +0000

    DON'T FORGET TO CREATE A MESSAGE!!!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//printing single line
+//from multiple lines of code
+#include <iostream> //enables input and output
+
+//function to leverage multi-level execution of multiple lines
+//to produce single line of output
+int main(){
+    std::cout<<"Hey James,\n";
+    std::cout<<"I am back successfully!\n";
+}//end  of function main
